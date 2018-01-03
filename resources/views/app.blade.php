fe2f7e2719346c3385dde6217de219ae9c4712a7
Robert Wittman <rob@ihsdigital.com>
2018-01-03 11:05:08 -0700

    Add polaris stylesheet

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel React application</title>
+        <link rel="stylesheet" href="https://sdks.shopifycdn.com/polaris/1.9.1/polaris.min.css" />
         <link href="/css/app.css" rel="stylesheet" type="text/css">
     </head>
     <body>
