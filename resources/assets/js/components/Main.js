04decfc7e3eb521799e40f471eb7e9b63418a7fd
Robert Wittman <rob@ihsdigital.com>
2018-01-03 11:15:09 -0700

    Add missing packages, fix app layout

--- resources/assets/js/components/Main.js
+++ resources/assets/js/components/Main.js
@@ -1,13 +1,45 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+
+import createHistory from 'history/createBrowserHistory'
+import { Route } from 'react-router'
+
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
+
+import reducers from '../reducers' // Or wherever you keep your reducers
+
+// Create a history of your choosing (we're using a browser history in this case)
+const history = createHistory()
+
+// Build the middleware for intercepting and dispatching navigation actions
+const middleware = routerMiddleware(history)
+
+// Add the reducer to your store on the `router` key
+// Also apply our middleware for navigating
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    router: routerReducer
+  }),
+  applyMiddleware(middleware)
+)
 
 /* An example React component */
 class Main extends Component {
     render() {
         return (
-            <div>
-                <h3>All Products</h3>
-            </div>
+            <Provider store={store}>
+               <ConnectedRouter history={history}>
+                 <div>
+                   <Route exact path="/" component={Home}/>
+                   <Route path="/about" component={About}/>
+                   <Route path="/topics" component={Topics}/>
+                 </div>
+               </ConnectedRouter>
+             </Provider>
         );
     }
 }
