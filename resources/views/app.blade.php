eb8ed1a09e58e8c7d599d54c7d900763340c149d
Rob Wittman <rob@ihsdigital.com>
2018-01-07 10:26:00 -0700

    Add _config to layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,9 +7,14 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel React application</title>
         <link rel="stylesheet" href="https://sdks.shopifycdn.com/polaris/1.9.1/polaris.min.css" />
-        <link href="/css/app.css" rel="stylesheet" type="text/css">
     </head>
     <body>
+        <script type='text/javascript'>
+            var _config = {
+                apiKey: "{{ config('shopify.api_key')}}"
+            }
+            console.log(_config);
+        </script>
         <div id="root"></div>
         <script src="/js/app.js" ></script>
     </body>
